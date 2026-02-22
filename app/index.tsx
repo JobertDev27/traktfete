9c35a584c23ff09afb6c6e3a8986dfc103000629
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-02-23 00:04:40 +0800

    feat: flatlist render from data

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,57 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const spendings = [
+  {
+    id: 1,
+    spendOn: "water",
+    price: 15,
+  },
+  {
+    id: 2,
+    spendOn: "fishball",
+    price: 25,
+  },
+  {
+    id: 3,
+    spendOn: "siomai rice",
+    price: 35,
+  },
+  {
+    id: 4,
+    spendOn: "beef tafa",
+    price: 50,
+  },
+  {
+    id: 5,
+    spendOn: "gas",
+    price: 100,
+  },
+];
+
+interface itemType {
+  spendOn: String;
+  price: number;
+}
+
+const ItemBox = ({ spendOn, price }: itemType) => {
+  return (
+    <View>
+      <Text>{spendOn}</Text>
+      <Text>{price}</Text>
+    </View>
+  );
+};
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <FlatList
+          data={spendings}
+          renderItem={({ item }) => <ItemBox {...item} />}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
